1e5497471385f2c2944d6851f9982d4e36428f83
Ivan Orfanidi <i.orfanidi@gaskar.group>
2021-12-20 19:01:37 +0300

    Добавлен пример получения версии из git коммитов с помощью cmake.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Compile-time version strings in CMake
+
+* https://www.mattkeeter.com/blog/2018-01-06-versioning/
---
+++ source/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+extern const char* GIT_TAG;
+extern const char* GIT_REV;
+extern const char* GIT_BRANCH;
+
+int main()
+{
+    std::cout << "git branch: \'" << GIT_BRANCH << '\'' << std::endl;
+    std::cout << "git revision: \'" << GIT_REV << '\'' << std::endl;
+    std::cout << "git tag: \'" << GIT_TAG << '\'' << std::endl;
+    return EXIT_SUCCESS;
+}
